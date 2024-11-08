a6ad9df3acbd7779b59dc7c72592709ed0338e03
Rafael Hoffmann <rafaelhfallgatter@gmail.com>
2024-11-08 16:42:18 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,8 +4,8 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="test-bucket-rhfall"
+export GCP_PROJECT="project-id-3187519002330642642"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
